cacd9db31c474a6798f835c2b396d3082a316df8
hawk <18801353760@163.com>
2022-10-03 22:59:53 +0800

    edit: refactor the echo in ~/.msmtprc
    To make code more clearly, echo only prints one line.

--- email.sh
+++ email.sh
@@ -3,13 +3,13 @@ set -e
 set -x
 
 usage() {
-	echo "Usage:"
-	echo "	$0 <git name> <git email> <gmail password> <linux source code path>"
+        echo "Usage:"
+        echo "        $0 <git name> <git email> <gmail password> <linux source code path>"
 }
 
 if [[ $# != 4 ]]; then
-	usage
-	exit 1
+        usage
+        exit 1
 fi
 
 port=587
@@ -35,38 +35,39 @@ sudo apt-get install msmtp
 # set msmtp config
 # content of ~/.msmtprc
 #
-#	defaults
-#	auth            on
-#	tls             on
-#	tls_starttls	on
-#	tls_trust_file  /etc/ssl/certs/ca-certificates.crt
-#	logfile         ~/.msmtp.log
+#        defaults
+#        auth                      on
+#        tls                       on
+#        tls_starttls              on
+#        tls_trust_file            /etc/ssl/certs/ca-certificates.crt
+#        logfile                   ~/.msmtp.log
 #
-#	#Gmail Account:
-#	account			  gmail
-#	host			  smtp.gmail.com
-#	port			  ${port}
-#	from			  ${email}
-#	user			  ${email}
-#	password		  ${password}
+#        #Gmail Account:
+#        account                   gmail
+#        host                      smtp.gmail.com
+#        port                      ${port}
+#        from                      ${email}
+#        user                      ${email}
+#        password                  ${password}
 #
-#	# Set a default account:
-#	account default : gmail
+#        # Set a default account:
+#        account default :         gmail
 #
-echo -n "defaults
-auth            on
-tls             on
-tls_starttls	on
-tls_trust_file  /etc/ssl/certs/ca-certificates.crt
-logfile         ~/.msmtp.log
-
-#Gmail Account:
-account			  gmail
-host			  smtp.gmail.com
-port			  ${port}
-from			  ${email}
-user			  ${email}
-password		  ${password}
-
-# Set a default account:
-account default : gmail" > ~/.msmtprc
+echo -n ''                                                               >  ~/.msmtprc
+echo 'defaults'                                                          >> ~/.msmtprc
+echo 'auth                      on'                                      >> ~/.msmtprc
+echo 'tls                       on'                                      >> ~/.msmtprc
+echo 'tls_starttls              on'                                      >> ~/.msmtprc
+echo 'tls_trust_file            /etc/ssl/certs/ca-certificates.crt'      >> ~/.msmtprc
+echo 'logfile                   ~/.msmtp.log'                            >> ~/.msmtprc
+echo ''                                                                  >> ~/.msmtprc
+echo '#Gmail Account:'                                                   >> ~/.msmtprc
+echo 'account                   gmail'                                   >> ~/.msmtprc
+echo 'host                      smtp.gmail.com'                          >> ~/.msmtprc
+echo 'port                      '"${port}"                               >> ~/.msmtprc
+echo 'from                      '"${email}"                              >> ~/.msmtprc
+echo 'user                      '"${email}"                              >> ~/.msmtprc
+echo 'password                  '"${password}"                           >> ~/.msmtprc
+echo ''                                                                  >> ~/.msmtprc
+echo '# Set a default account:'                                          >> ~/.msmtprc
+echo 'account default :         gmail'                                   >> ~/.msmtprc
